2cadcfde1d487b5a432ff4c369190b635df04029
mlekena_Theko <mlekena@skidmore.edu>
2015-05-16 16:02:07 +1000

    added the predictor classes

---
+++ WeatherPredictions/app/predictors/RainFallPredictor.rb
@@ -0,0 +1,12 @@
+#RainFallPredictor.rb
+
+class RainFallPredictor < RainFallPredictor
+
+	def initialize _name
+		super _name
+	end
+
+	def predict _p
+		
+	end
+end
---
+++ WeatherPredictions/app/predictors/predictor.rb
@@ -0,0 +1,19 @@
+#Predictor Class file
+
+class Predictor
+	attr_accessor :weather_feature
+
+	def initialize _name
+		@weather_feature = _name
+	end
+
+	def predict _p
+		p.predictors.each do |preditor|
+			
+		end#end of predictor calls
+	end
+
+	def regress
+
+	end
+end	

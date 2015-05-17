0d23ecdd91fb77bb37d806d37022c180e20cba4d
adamthompson93 <adamthompson@utexas.edu>
2015-05-17 21:10:59 +1000

    added some forecast functionality
    added a get_date function in the weather station model, started work on
    the data initializer. I'm still not sure whether also using BOM is worth
    the trouble.

--- WeatherPredictions/app/models/weather_station.rb
+++ WeatherPredictions/app/models/weather_station.rb
@@ -1,3 +1,18 @@
+require 'forecast_io'
+require 'time'
+ForecastIO.api_key = '811a6d564ecf0a130c1becb4ec00ee6c'
+
 class WeatherStation < ActiveRecord::Base
   has_many :measurements
+
+  def get_date(date)
+  	time = Date.parse(date)
+  	forecast = ForecastIO.forecast(self.lat, self.lon, time: time.to_i)
+  	data = forecast.hourly
+  	data.each do |point|
+  	measurement = self.measurements.create(time: Time.parse(point.time), temperature: point.temperature, 
+  		wind_direction: point.windDirection, wind_speed: point.windSpeed, precipitation: point.precipIntensity)
+  	measurement.save
+
+  end
 end

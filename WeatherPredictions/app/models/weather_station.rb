b010b2faa4863144280f947b634ae71801592b56
adamthompson93 <adamthompson@utexas.edu>
2015-05-25 20:22:30 +1000

    unfinished update to data scraper
    Urgh. Don't worry about it. Had to switch from weather_io to BOM

--- WeatherPredictions/app/models/weather_station.rb
+++ WeatherPredictions/app/models/weather_station.rb
@@ -5,37 +5,11 @@ ForecastIO.api_key = '811a6d564ecf0a130c1becb4ec00ee6c'
 class WeatherStation < ActiveRecord::Base
   has_many :measurements
 
-  def get_data_at_time(time)
+  #dates must be passed as strings in the form of DD/MM/YYYY
+  def get_date(date)
+  	time = Date.parse(date)
     forecast = ForecastIO.forecast(self.lat, self.lon, time: time.to_i)
     data = forecast.data.hourly
-    data.each do |point|
-      measurement = self.measurements.create(condition: point.icon.to_s, time: Time.parse(point.time), temperature: point.temperature, 
-        wind_direction: point.windDirection, wind_speed: point.windSpeed, precipitation: point.precipIntensity)
-      measurement.save
-    end
-  end
-
-  #dates must be passed as strings in the form of DD/MM/YYYY
-  def get_data_at_date(date)
-  	time = Date.parse(date)
-  	get_data_at_time(time)
-  end
-
-  def get_recent()
-  	time = Time.now
-    forecast = ForecastIO.forecast(self.lat, self.lon, time: time.to_i - 3600)
-    data = forecast.data.minutely
-    data.each do |point|
-      measurement = self.measurements.create(condition: point.icon.to_s, time: Time.parse(point.time), temperature: point.temperature, 
-        wind_direction: point.windDirection, wind_speed: point.windSpeed, precipitation: point.precipIntensity)
-      measurement.save
-    end
-  end
-
-  def get_current()
-    time = Time.now
-    forecast = ForecastIO.forecast(self.lat, self.lon, time: time.to_i)
-    data = forecast.data.currently
     data.each do |point|
       measurement = self.measurements.create(condition: point.icon.to_s, time: Time.parse(point.time), temperature: point.temperature, 
         wind_direction: point.windDirection, wind_speed: point.windSpeed, precipitation: point.precipIntensity)

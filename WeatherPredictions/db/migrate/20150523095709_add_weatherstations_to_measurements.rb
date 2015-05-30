306420002a7b45cfe9a7b06f507d149496dcd629
Adam Thompson <adamthompson@utexas.edu>
2015-05-30 17:06:23 +1000

    bugfixes v.2
    db issues

--- WeatherPredictions/db/migrate/20150523095709_add_weatherstations_to_measurements.rb
+++ WeatherPredictions/db/migrate/20150523095709_add_weatherstations_to_measurements.rb
@@ -1,6 +1,6 @@
 class AddWeatherstationsToMeasurements < ActiveRecord::Migration
   def change
-    add_reference :measurements, :weatherstation, index: true
-    add_foreign_key :measurements, :weatherstations
+    add_reference :measurements, :weather_station, index: true
+    add_foreign_key :measurements, :weather_stations
   end
 end
